14df45b6df1e73fb9b08ce81bab4e70423c75ae0
Temur Garayev <tgarayev@myseneca.ca>
2024-10-09 22:13:00 -0400

    Linked List Implemetation

--- LinkedList.py
+++ LinkedList.py
@@ -2,7 +2,7 @@ class LinkedList:
     class Node:
         def __init__(self, data, next=None, prev=None):
             self.data = data
-            self.next = next
+            self.next = next 
             self.prev = prev
     def __init__(self):
         self.front = None
